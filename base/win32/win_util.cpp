2de54e3bada05403779390d65159563d08cd55c1
heping.hm <southwindod@foxmail.com>
2021-04-11 18:55:37 +0800

    base : fix warning 4244;4312

--- base/win32/win_util.cpp
+++ base/win32/win_util.cpp
@@ -18,7 +18,7 @@ namespace win32
 HMODULE GetModuleHandleFromAddress(void* address)
 {
 	MEMORY_BASIC_INFORMATION mbi = {0};
-	DWORD result = ::VirtualQuery(address, &mbi, sizeof(mbi));
+	DWORD result = (DWORD)::VirtualQuery(address, &mbi, sizeof(mbi));
 	assert(result == sizeof(mbi));
 	return static_cast<HMODULE>(mbi.AllocationBase);
 }
