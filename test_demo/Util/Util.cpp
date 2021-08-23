29d97b302afa526d5851153d6c902d67d13c85e5
heping.wk <southwindod@foxmail.com>
2021-08-23 10:43:36 +0800

    test_demo工程 添加Util

---
+++ test_demo/Util/Util.cpp
@@ -0,0 +1,25 @@
+﻿#include "stdafx.h"
+#include "Util.h"
+
+//****************************/
+//-- class Util
+//****************************/
+//////////////////////////////////////////////////////////////////////////
+
+std::wstring Util::GetAppDir()
+{
+	static std::wstring wstrAppDir;
+	if (wstrAppDir.empty())
+	{
+		WCHAR pBuf[MAX_PATH + 1] = { 0 };
+		DWORD dwModLen = ::GetModuleFileNameW((HMODULE)ATL::_AtlBaseModule.GetModuleInstance(), pBuf, MAX_PATH + 1);
+		if (dwModLen > 0 && dwModLen < MAX_PATH) {
+			WCHAR* pDirEnd = wcsrchr(pBuf, L'\\');
+			if (pDirEnd != NULL) {
+				pBuf[pDirEnd + 1 - pBuf] = '\0';
+				wstrAppDir = pBuf;
+			}
+		}
+	}
+	return wstrAppDir;
+}
---
+++ test_demo/Util/Util.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+#include <string>
+
+//****************************/
+//-- class Util
+//****************************/
+class Util
+{
+public:
+	// 获取exe所在目录 注：尾部带有"\\"
+	static std::wstring GetAppDir();
+};
