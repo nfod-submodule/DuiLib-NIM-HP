0d8bb465231d12a87fd4d4ae0ed3fd979574f97b
heping.wk <southwindod@foxmail.com>
2021-08-30 13:35:44 +0800

    test_demo工程 完善 MainForm - 添加退出确认

--- test_demo/UI_Form/MainForm.cpp
+++ test_demo/UI_Form/MainForm.cpp
@@ -2,6 +2,7 @@
 #include "Resource.h"
 #include "MainForm.h"
 #include "LoginKit.h"
+#include "MsgBox.h"
 
 //****************************/
 //-- namespace NS_MainForm
@@ -15,8 +16,15 @@
 NS_MainForm_BEGIN
 
 //-- 控件名称
+CONST PWSTR WGT_closebtn2	= L"closebtn2";		// Button: 关闭（窗口右上角叉）
 CONST PWSTR WGT_btn_logout	= L"btn_logout";	// Button: 注销
 CONST PWSTR WGT_btn_exit	= L"btn_exit";		// Button: 退出
+
+//-- 文本内容
+CONST PWSTR TXT_exit_confirm	= L"确定退出程序？";
+CONST PWSTR TXT_title_hint		= L"提示";
+CONST PWSTR TXT_btn_confirm		= L"确定";
+CONST PWSTR TXT_btn_cancel		= L"取消";
 
 NS_MainForm_END
 USING_NS_MainForm;
@@ -65,13 +73,22 @@ bool MainForm::OnNotify(ui::EventArgs* msg)
 bool MainForm::OnClicked(ui::EventArgs* msg)
 {
 	std::wstring sSenderName = msg->pSender->GetName();
-	if (sSenderName == WGT_btn_logout)
+	if (sSenderName == WGT_closebtn2)
+	{
+		auto cb_Close = [this](ui_comp::EMsgBoxRet eRet) {
+			if (eRet == ui_comp::eMsgBox_YES) {
+				this->Close();
+			}
+		};
+		ui_comp::MsgBox::Show(m_hWnd, ToWeakCallback(cb_Close), TXT_exit_confirm, TXT_title_hint, TXT_btn_confirm, TXT_btn_cancel);
+	}
+	else if (sSenderName == WGT_btn_logout)
 	{
 		LoginKit::GetInstance()->DoLogout();
 	}
 	else if (sSenderName == WGT_btn_exit)
 	{
-		;
+		this->Close();
 	}
 	return true;
 }
